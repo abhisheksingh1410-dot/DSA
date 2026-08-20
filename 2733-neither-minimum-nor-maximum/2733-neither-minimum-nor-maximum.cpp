ca17fe9e8cc4654ba498abe89f50b2f60b37403c
ABHISHEK SINGH <abhithakur142003@gmail.com>
2026-08-20 15:34:18 +0530

    Time: 5 ms (40.88%), Space: 97.2 MB (60.98%) - LeetHub

---
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()>2){
+            return nums[1];
+
+        }else{
+            return -1;
+        }
+        
+    }
+};
