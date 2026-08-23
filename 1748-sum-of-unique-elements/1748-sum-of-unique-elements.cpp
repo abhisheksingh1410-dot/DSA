4958b4929b6d0bfe8b97f344e7a45333c1417c0e
ABHISHEK SINGH <abhithakur142003@gmail.com>
2026-08-23 13:58:02 +0530

    Time: 0 ms (100%), Space: 10.8 MB (18.6%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(auto x:nums){
+            freq[x]++;
+
+        }
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]]==1){
+                sum+=nums[i];
+
+                
+            }
+        }
+
+    
+       return sum;
+        
+    }
+};
