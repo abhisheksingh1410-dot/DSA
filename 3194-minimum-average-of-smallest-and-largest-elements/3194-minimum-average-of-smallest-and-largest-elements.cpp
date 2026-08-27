a56959234ca9826fd64aab18fc900d52120d757a
ABHISHEK SINGH <abhithakur142003@gmail.com>
2026-08-27 14:32:17 +0530

    Time: 1 ms (18.95%), Space: 26 MB (7.09%) - LeetHub

---
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        vector<float> avg;
+        sort(nums.begin(), nums.end());
+        float avrg = 0.0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            avrg =( nums[i] + nums[nums.size() - (i + 1)])/2.0;
+            avg.push_back(avrg);
+
+            
+        }
+        sort(avg.begin(), avg.end());
+        return avg[0];
+    }
+};
