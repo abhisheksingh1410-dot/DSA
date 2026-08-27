3c57cf7de5f1c16f91ddfa91e3b6b7e264444daa
ABHISHEK SINGH <abhithakur142003@gmail.com>
2026-08-27 14:33:46 +0530

    Time: 4 ms (5.49%), Space: 25.6 MB (56.51%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         float avrg = 0.0;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size()/2; i++) {
             avrg =( nums[i] + nums[nums.size() - (i + 1)])/2.0;
             avg.push_back(avrg);
 
